170e293448ecab458eac0370d0a1a4601d3dd350
TheShadow88 <stef4oben88@gmail.com>
2016-09-20 18:00:54 +0300

    Done

--- phone transformer.py
+++ phone transformer.py
@@ -1,9 +1,12 @@
 import re
 file = open("run4hackz.py","r+")
 txt = file.read()
+print("Enter the patch where you want to save your game")
 patch = input("Enter full patch default(/storage/emulated/0/games/db.txt): ")
 if patch == "":
     patch = "/storage/emulated/0/games/db.txt"
+elif patch[-3:] != "txt":
+    patch += "db.txt"
 txt = re.sub(r"db.txt",patch,txt)
 file.truncate()
 file.write(txt)
